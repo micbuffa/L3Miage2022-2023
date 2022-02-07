6d1d18e5736d8046441dad092b9533cf3acd208b
micbuffa <micbuffa@gmail.com>
2022-02-07 12:05:49 +0100

    mise a jout exemple sauvegarde hiscore seance4

--- COURS/Seance4/exemple_api_WS/serveur.js
+++ COURS/Seance4/exemple_api_WS/serveur.js
@@ -1,4 +1,7 @@
 var express = require("express");
+var multer = require("multer");
+var multerData = multer();
+
 var app = express();
 
 app.use(express.static("public")); // ICI IMPORTANT !
@@ -7,13 +10,22 @@ app.get("/", function (req, res) {
     res.sendFile(__dirname + "/public/index.html");
 });
 
-app.get("/hiscores", function (req, res) {
+app.get("/api/hiscores", function (req, res) {
     res.send("Hi Scores :");
 
 });
 
-app.post("/hiscores", function (req, res) {
-    res.send("Hi Score ajouté !!!!");
+app.post("/api/hiscores", multerData.fields([]), function (req, res) {
+    console.log("Hi Score ajouté !!!!");
+    console.log("post sur /api/hiscores, j'ai reçu :");
+    console.log(req.body.nom);
+    console.log(req.body.val);
+
+    const reponse = {
+        msg: "Server a reçu score de " + req.body.nom + " valeur: " + req.body.val,
+        code: "OK"
+    }
+    res.send(JSON.stringify(reponse));
 });
 
 var server = app.listen(8085, function () {
